7cb6ce2b692c130107b0e74348aa72dd00c29c33
Dominique d'Argent <dominique.dargent@me.com>
2014-06-04 00:23:51 +0200

    Set up Xcode project

---
+++ VerbalExpressions/VerbalExpressions.h
@@ -0,0 +1,19 @@
+//
+//  VerbalExpressions.h
+//  VerbalExpressions
+//
+//  Created by Dominique d'Argent on 04/06/14.
+//  Copyright (c) 2014 Dominique d'Argent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VerbalExpressions.
+FOUNDATION_EXPORT double VerbalExpressionsVersionNumber;
+
+//! Project version string for VerbalExpressions.
+FOUNDATION_EXPORT const unsigned char VerbalExpressionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerbalExpressions/PublicHeader.h>
+
+
---
+++ VerbalExpressions/VerbalExpressions.swift
@@ -0,0 +1,9 @@
+//
+//  VerbalExpressions.swift
+//  VerbalExpressions
+//
+//  Created by Dominique d'Argent on 04/06/14.
+//  Copyright (c) 2014 Dominique d'Argent. All rights reserved.
+//
+
+import Foundation
